2a2dd804a33a089e24d1091a0312730c94c251ae
morpheuszein <morpheus.zein@irawan.id>
2023-10-10 21:36:36 +0700

    Finishing Touches

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Maincolor {
+  static Color primaryColor = const Color(0xFF331B3F);
+  static Color secColor = const Color(0xFFACC7B4);
+  static Color accentColor = const Color(0xFF4169e8);
+}
